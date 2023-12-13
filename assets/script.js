16f01f29cd358429e4cc19d5c18e1f311f8201e1
Ugur-F <ugur.firat240@gmail.com>
2023-12-13 15:55:10 +0100

    initialisation du fCollection

---
+++ assets/script.js
@@ -0,0 +1,84 @@
+const fCollection = [
+    {
+        name: "Khabib Nurmagomedov",
+        nickname: "The Eagle",
+        nationality: "Daghestans",
+        picture: url(assets/img/athlete/khabib.png),
+        division: "Lightweight",
+    },
+
+    {
+        name: "Charles Oliveira",
+        nickname: "Do Bronxs",
+        nationality: "Brésil",
+        picture: url(assets/img/athlete/charlesO.png),
+        division: "Lightweight",
+    },
+
+    {
+        name: "Francis Ngannou",
+        nickname: "The Predator",
+        nationality: "Cameroun",
+        picture: url(assets/img/athlete/francisN.png),
+        division: "Heavyweight",
+    },
+
+    {
+        name: "Jon Jones",
+        nickname: "Bones",
+        nationality: "Etat-Unis",
+        picture: url(assets/img/athlete/jonJ.png),
+        division: "Heavyweight",
+    },
+
+    {
+        name: "Demetrious Johnson",
+        nickname: "Mighty Mouse",
+        nationality: "Etat-Unis",
+        picture: url(assets/img/athlete/demetriousJ.png),
+        division: "Flyweight",
+    },
+
+    {
+        name: "Kamaru Usman",
+        nickname: "Nigerian Nightmare",
+        nationality: "Nigeria",
+        picture: url(assets/img/athlete/kamrauU.png),
+        division: "Middleweight",
+    },
+
+    {
+        name: "Israel Adesanya",
+        nickname: "The Last Stylebender",
+        nationality: "Nigéria",
+        picture: url(assets/img/athlete/khabib.png),
+        division: "Middleweight",
+    },
+    
+    {
+        name: "Khamzat Chimaev",
+        nickname: "Borz",
+        nationality: "Suède",
+        picture: url(assets/img/athlete/khamzatC.png),
+        division: "Middleweight",
+    },
+
+    {
+        name: "Max Holloway",
+        nickname: "Blessed",
+        nationality: "Etat-Unis",
+        picture: url(assets/img/athlete/maxH.png),
+        division: "Featherweight",
+    },
+
+    {
+        name: "Dustin Poirier",
+        nickname: "The Diamond",
+        nationality: "Etat-Unis",
+        picture: url(assets/img/athlete/dustinP.png),
+        division: "Lightweight",
+    },
+]
+
+//=========================================================================================================
+
