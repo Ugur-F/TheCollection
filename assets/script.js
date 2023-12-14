6bcb57998a685d38aa9624ac96da0980f796be5d
Ugur-F <ugur.firat240@gmail.com>
2023-12-14 09:10:31 +0100

    mise en place du css

--- assets/script.js
+++ assets/script.js
@@ -2,83 +2,85 @@ const fCollection = [
     {
         name: "Khabib Nurmagomedov",
         nickname: "The Eagle",
-        nationality: "Daghestans",
-        picture: url(assets/img/athlete/khabib.png),
+        nationality: "assets/img/flag/russie.png",
+        picture: "assets/img/athlete/khabib.png",
         division: "Lightweight",
     },
 
     {
         name: "Charles Oliveira",
         nickname: "Do Bronxs",
-        nationality: "Brésil",
-        picture: url(assets/img/athlete/charlesO.png),
+        nationality: "assets/img/flag/bresil.png",
+        picture: "assets/img/athlete/charlesO.png",
         division: "Lightweight",
     },
 
     {
         name: "Francis Ngannou",
         nickname: "The Predator",
-        nationality: "Cameroun",
-        picture: url(assets/img/athlete/francisN.png),
+        nationality: "assets/img/flag/cameroun.png",
+        picture: "assets/img/athlete/francisN.png",
         division: "Heavyweight",
     },
 
     {
         name: "Jon Jones",
         nickname: "Bones",
-        nationality: "Etat-Unis",
-        picture: url(assets/img/athlete/jonJ.png),
+        nationality: "assets/img/flag/etat-unis.png",
+        picture: "assets/img/athlete/jonJ.png",
         division: "Heavyweight",
     },
 
     {
         name: "Demetrious Johnson",
         nickname: "Mighty Mouse",
-        nationality: "Etat-Unis",
-        picture: url(assets/img/athlete/demetriousJ.png),
+        nationality: "assets/img/flag/etat-unis.png",
+        picture: "assets/img/athlete/demetriousJ.png",
         division: "Flyweight",
     },
 
     {
         name: "Kamaru Usman",
         nickname: "Nigerian Nightmare",
-        nationality: "Nigeria",
-        picture: url(assets/img/athlete/kamrauU.png),
+        nationality: "assets/img/flag/cameroun.png",
+        picture: "assets/img/athlete/kamrauU.png",
         division: "Middleweight",
     },
 
     {
         name: "Israel Adesanya",
         nickname: "The Last Stylebender",
-        nationality: "Nigéria",
-        picture: url(assets/img/athlete/khabib.png),
+        nationality: "assets/img/flag/nigeria.png",
+        picture: "assets/img/athlete/khabib.png",
         division: "Middleweight",
     },
     
     {
         name: "Khamzat Chimaev",
         nickname: "Borz",
-        nationality: "Suède",
-        picture: url(assets/img/athlete/khamzatC.png),
+        nationality: "assets/img/flag/suede.png",
+        picture: "assets/img/athlete/khamzatC.png",
         division: "Middleweight",
     },
 
     {
         name: "Max Holloway",
         nickname: "Blessed",
-        nationality: "Etat-Unis",
-        picture: url(assets/img/athlete/maxH.png),
+        nationality: "assets/img/flag/etat-unis.png",
+        picture: "assets/img/athlete/maxH.png",
         division: "Featherweight",
     },
 
     {
         name: "Dustin Poirier",
         nickname: "The Diamond",
-        nationality: "Etat-Unis",
-        picture: url(assets/img/athlete/dustinP.png),
+        nationality: "assets/img/flag/etat-unis.png",
+        picture: "assets/img/athlete/dustinP.png",
         division: "Lightweight",
     },
 ]
 
 //=========================================================================================================
+
+console.log("test");
 
